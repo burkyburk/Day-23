c5528e7a49b83c7b1092fc51284f33afef75a596
Jacob Burkhart <jacobdouglasburkhart@gmail.com>
2015-02-10 21:59:36 -0600

    updates

--- tests/tests.js
+++ tests/tests.js
@@ -32,17 +32,83 @@ describe('PieceView', function() {
 
 		expect($box2.css('top')).to.equal('60px');
 		expect($box2.css('left')).to.equal('20px');
+	});
 
-		var $box3 = myPieceView.$el.children().eq(2);
+	it('should render a piece of type=0 and rotation=1 correctly', function() {
+		var myPieceModel = new PieceModel({
+			type: 0,
+			rotation: 1
+		});
+		var myPieceView = new PieceView({model: myPieceModel});
+		myPieceView.render();
 
-		expect($box3.css('top')).to.equal('60px');
-		expect($box3.css('left')).to.equal('40px');
+		var $box1 = myPieceView.$el.children().eq(0);
 
-		var $box4 = myPieceView.$el.children().eq(3);
+		expect($box1.css('top')).to.equal('0px');
+		expect($box1.css('left')).to.equal('20px');
 
-		expect($box4.css('top')).to.equal('60px');
-		expect($box4.css('left')).to.equal('60px');
-	})
+		var $box2 = myPieceView.$el.children().eq(1);
+
+		expect($box2.css('top')).to.equal('20px');
+		expect($box2.css('left')).to.equal('20px');
+	});
+
+	it('should render a piece of type=2 and rotation=1 correctly', function() {
+		var myPieceModel = new PieceModel({
+			type: 2,
+			rotation: 1
+		});
+		var myPieceView = new PieceView({model: myPieceModel});
+		myPieceView.render();
+
+		var $box1 = myPieceView.$el.children().eq(0);
+
+		expect($box1.css('top')).to.equal('0px');
+		expect($box1.css('left')).to.equal('20px');
+
+		var $box2 = myPieceView.$el.children().eq(1);
+
+		expect($box2.css('top')).to.equal('20px');
+		expect($box2.css('left')).to.equal('20px');
+	});
+
+	it('should render a piece of type=1 and rotation=0 correctly', function() {
+		var myPieceModel = new PieceModel({
+			type: 1,
+			rotation: 0
+		});
+		var myPieceView = new PieceView({model: myPieceModel});
+		myPieceView.render();
+
+		var $box1 = myPieceView.$el.children().eq(0);
+
+		expect($box1.css('top')).to.equal('0px');
+		expect($box1.css('left')).to.equal('0px');
+
+		var $box2 = myPieceView.$el.children().eq(1);
+
+		expect($box2.css('top')).to.equal('0px');
+		expect($box2.css('left')).to.equal('20px');
+	});
+
+	it('should render a piece of type=3 and rotation=0 correctly', function() {
+		var myPieceModel = new PieceModel({
+			type: 3,
+			rotation: 0
+		});
+		var myPieceView = new PieceView({model: myPieceModel});
+		myPieceView.render();
+
+		var $box1 = myPieceView.$el.children().eq(0);
+
+		expect($box1.css('top')).to.equal('20px');
+		expect($box1.css('left')).to.equal('0px');
+
+		var $box2 = myPieceView.$el.children().eq(1);
+
+		expect($box2.css('top')).to.equal('40px');
+		expect($box2.css('left')).to.equal('0px');
+	});
 });
 
 describe('PieceView.$el', function() {
@@ -65,6 +131,9 @@ describe('PieceView.$el', function() {
 	});
 });
 
+
+
+
 describe('PieceModel', function() {
 
 	var myPiece = new PieceModel();
@@ -75,6 +144,14 @@ describe('PieceModel', function() {
 
 	it('should have a move method', function() {
 		expect(myPiece.move).to.exist();
+	});
+
+	it('should have a width method', function() {
+		expect(myPiece.width).to.exist();
+	});
+
+	it('should have a height method', function() {
+		expect(myPiece.height).to.exist();
 	});
 
 	it('should have a rotate method', function() {
@@ -99,6 +176,10 @@ describe('PieceModel', function() {
 
 	it('should have a bottom method', function() {
 		expect(myPiece.top).to.exist();
+	});
+
+	it('should have a iterateMatrix method', function() {
+		expect(myPiece.iterateMatrix).to.exist();
 	});
 
 	it('should have a type property', function() {
@@ -151,25 +232,133 @@ describe('PieceModel', function() {
 			rotation: 0
 		});
 		expect(piece1.left()).to.equal(0);
-		// expect(piece1.right()).to.equal(4);
-		// expect(piece1.bottom()).to.equal(4);
-		
 
 		var piece2 = new PieceModel({
 			type: 0,
 			rotation: 1
 		});
 		expect(piece2.left()).to.equal(1);
-		// expect(piece2.right()).to.equal(2);
-		// expect(piece2.bottom()).to.equal(4);
-		// expect(piece2.top()).to.equal(0);
+	});
+
+	it('should properly calculate the right value', function(){
+		var piece1 = new PieceModel({
+			type: 0,
+			rotation: 2
+		});
+
+		expect(piece1.right()).to.equal(4);
+
+		var piece1 = new PieceModel({
+			type: 0,
+			rotation: 1
+		});
+
+		expect(piece1.right()).to.equal(2);
+	});
+
+	it('should properly calulate the bottom value', function() {
+		var peice1 = new PieceModel({
+			type: 0,
+			rotation: 3
+		});
+
+		expect(peice1.bottom()).to.equal(4);
+
+		var peice1 = new PieceModel({
+			type: 1,
+			rotation: 3
+		});
+
+		expect(peice1.bottom()).to.equal(2);
 	});
 
 	it('should properly calculate the top value', function() {
 		var piece1 = new PieceModel({
 			type: 0,
+			rotation: 1
+		});
+		expect(piece1.top()).to.equal(0);
+
+		var piece1 = new PieceModel({
+			type: 1,
+			rotation: 1
+		});
+		expect(piece1.top()).to.equal(0);
+
+		var piece1 = new PieceModel({
+			type: 2,
 			rotation: 0
 		});
 		expect(piece1.top()).to.equal(0);
 	});
+
+
+	it('should calculate width correctly', function() {
+		expect(myPiece.width()).to.equal(4);
+
+		var piece3 = new PieceModel({
+			type: 1
+		});
+
+		expect(piece3.width()).to.equal(2);
+
+		var piece4 = new PieceModel({
+			type: 0,
+			rotation: 2
+		});
+
+		expect(piece4.width()).to.equal(4);
+	});
+
+	it('should calculate height properly', function() {
+		expect(myPiece.height()).to.equal(4);
+
+		var piece5 = new PieceModel({
+			type: 0,
+			rotation: 1
+		});
+		expect(piece5.height()).to.equal(4);
+
+		var piece6 = new PieceModel({
+			type: 1,
+			rotation: 1
+		});
+		expect(piece6.height()).to.equal(2);
+
+	});
+
+	it('should be able to calculate move properly', function(){
+		var piece1 = new PieceModel({
+			type: 0,
+			rotation: 1
+		});
+
+		piece1.move(2);
+		expect(piece1.attributes.left).to.equal(2);
+		piece1.move(6);
+		expect(piece1.attributes.left).to.equal(8);
+		piece1.move(-4);
+		expect(piece1.attributes.left).to.equal(4);
+	});
+
+	it('should calculate drop properly', function() {
+		var piece1 = new PieceModel({
+			type: 0,
+			rotation: 0
+		});
+
+		expect(piece1.attributes.age).to.equal(0);
+		piece1.drop();
+		expect(piece1.attributes.age).to.equal(1);
+		piece1.drop();
+		expect(piece1.attributes.age).to.equal(2);
+		piece1.drop();
+	});
+
+	it('should be able to iterative through the matrix properly', function(){
+		myPiece.iterateMatrix(function(x,y) {
+			expect(Constant.PIECES[0][0][y][x]).to.equal(1);
+		});
+	});
 });
+
